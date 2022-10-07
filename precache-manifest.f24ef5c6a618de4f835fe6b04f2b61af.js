75e6911d2fd2e3ddacecb253587996a4322bf507
Saki Unno <unx2o.sk@gmail.com>
2022-10-07 18:31:57 +0100

    Updates

---
+++ precache-manifest.f24ef5c6a618de4f835fe6b04f2b61af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d88784c70e7f630cff",
+    "url": "/todolist/static/css/main.5dcf925a.chunk.css"
+  },
+  {
+    "revision": "13d88784c70e7f630cff",
+    "url": "/todolist/static/js/main.13d88784.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/todolist/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "7372631245bdf377fbdf",
+    "url": "/todolist/static/js/2.73726312.chunk.js"
+  },
+  {
+    "revision": "3120f41e96263835fc74",
+    "url": "/todolist/static/js/runtime~main.3120f41e.js"
+  },
+  {
+    "revision": "de1677361bba4c71abf538d421ba19bd",
+    "url": "/todolist/index.html"
+  }
+];
